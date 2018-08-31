8b3221086646d757bd12c4c90d4d4afaf9c106d2
id <msgexec@gmail.com>
2018-08-31 17:46:25 +0300

    asdf

--- main.go
+++ main.go
@@ -1,9 +1,8 @@
 package newtestmod
 
-
 import "fmt"
 
 // Hi returns a friendly greeting
 func Hi(name string) string {
-	return fmt.Sprintf("Hi, %s", name)
+	return fmt.Sprintf("Hi, %s!", name)
 }
